7de6653938e4048ca40981a6591c346afc6ad38c
mikaylafreitas <48600226+mikaylafreitas@users.noreply.github.com>
2020-02-05 11:27:41 -0800

    Update Airplane.java

---
+++ src/Airplane.java
@@ -0,0 +1,40 @@
+
+public class Airplane extends Vehicle{
+	
+	public Airplane(){
+		this("No name Airplane");
+	}
+	
+	public Airplane(String name){
+		this(name, 500);
+	}
+	
+	public Airplane(String name, double costPerHour){
+		this(name, costPerHour, new Engine("PrattAndWhitney", "Kerosene"))
+	}
+	
+	public Airplane(String name, double costPerHour, Engine engine){
+		super(name, engine);
+		mCostPerHour = costPerHour;
+	}
+	
+	public Airplane(Airplane a){
+		this(a.getName(),a.getCostPerHour(),a.getEngine());
+	}
+	
+	@Override
+	public String getName(){
+		return super.getName();
+	}
+	
+	@Override
+	public Engine getEngine(){
+		return super.getEngine();
+	}
+	
+	public double getCostPerHour(){
+		return mCostPerHour;
+	}
+	
+	private double mCostPerHour;
+}
