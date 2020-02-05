4810e9b9b02c349edf4fcb473216962505364db2
mikaylafreitas <48600226+mikaylafreitas@users.noreply.github.com>
2020-02-05 11:52:42 -0800

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,11 +1,23 @@
+
 public class Car extends Vehicle{
 	
-	public Car(){//fix
-		this("No Name Car");
+	public Car(){
+		this("No name Car");
 	}
 	
 	public Car(String name){
-		this(name, )
+		this(name, 20);
 	}
-
+	
+	public Car(String name, double milesPerGallon){
+		this(name, milesPerGallon, new Engine("Chevy", "Gas"));
+	}
+	
+	public Car(String name, double milesPerGallon, Engine engine){
+		super(name, engine);
+		mMilesPerGallon = milesPerGallon;
+	}
+	
+	
+	private double mMilesPerGallon;
 }
