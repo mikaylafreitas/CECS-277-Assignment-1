3c6d93682abffb9b12d778a47f101a156aa3760e
mikaylafreitas <48600226+mikaylafreitas@users.noreply.github.com>
2020-02-05 12:15:33 -0800

    Update Engine.java

--- src/Engine.java
+++ src/Engine.java
@@ -1,41 +1,34 @@
 
-public class Engine 
-{
-	public Engine(String maker, String fuel)
-	{
+public class Engine {
+	
+	public Engine(String maker, String fuel){
 		mMaker = maker;
 		mFuel = fuel;
 	}
 	
-	public Engine(Engine e)
-	{
+	public Engine(Engine e){
 		this(e.getMaker(), e.getFuel());
 	}
 	
-	public String getMaker()
-	{
+	public String getMaker(){
 		return mMaker;
 	}
 	
-	public String getFuel()
-	{
+	public String getFuel(){
 		return mFuel;
 	}
 	
-	
 	@Override
-	public String toString()
-	{
+	public String toString(){
 		return "Engine: Maker = " + mMaker + ", Fuel = " + mFuel;
 	}
 	
-	
 	@Override
-	public boolean equals(Object other)
-	{
-		if(!(other instanceof Engine)) return false;
-		else
-		{
+	public boolean equals(Object other){
+		if(!(other instanceof Engine)){
+			return false;
+		}
+		else{
 			Engine e = (Engine)other;
 			return mMaker == e.mMaker && mFuel == e.mFuel;
 		}
